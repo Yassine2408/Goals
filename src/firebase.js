543d8f8c66812442c1ad8dcf600a823aaf975820
Your Name <your-email@example.com>
2025-01-05 20:25:05 +0100

    Update Firebase configuration to use environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,17 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: "goals-d7fbc.firebaseapp.com",
-  projectId: "goals-d7fbc",
-  storageBucket: "goals-d7fbc.firebasestorage.app",
-  messagingSenderId: "1039132747654",
-  appId: "1:1039132747654:web:850b452499ee483e0fae07",
-  measurementId: "G-HQQPBCBHG3"
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
